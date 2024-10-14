359ddb7ef75808a5be6cc418dd919baed029e020
akbarabdimuhammad <125510670+akbarabdimuhammad@users.noreply.github.com>
2024-10-14 18:42:02 +0700

    Add files via upload

---
+++ pp2_223040129_C/latihan dan tugas sesi 3/latihan1/HelloBorderLayout.java
@@ -0,0 +1,43 @@
+package latihan1;
+import java.awt.*;
+import javax.swing.*;
+
+public class HelloBorderLayout extends JFrame {
+    
+    public HelloBorderLayout() {
+        
+        JLabel labelPertanyaan = new JLabel("Apakah ibukota Indonesia?");
+        JLabel labelHasil = new JLabel("Jawab pertanyaan di atas");
+        JButton buttonA = new JButton("Jakarta");
+        JButton buttonB = new JButton("Bandung");
+        JButton buttonC = new JButton("Surabaya");
+        
+        
+        buttonA.addActionListener(e -> labelHasil.setText("Jawaban anda benar"));
+        
+        buttonB.addActionListener(e -> labelHasil.setText("Jawaban anda salah"));
+        
+        buttonC.addActionListener(e -> labelHasil.setText("Jawaban anda salah"));
+        
+        
+        this.add(labelPertanyaan, BorderLayout.NORTH);
+        this.add(labelHasil, BorderLayout.SOUTH);
+        this.add(buttonA, BorderLayout.WEST);
+        this.add(buttonB, BorderLayout.CENTER);
+        this.add(buttonC, BorderLayout.EAST);
+        
+        
+        this.setSize(400, 200);
+        
+    
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+       
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            HelloBorderLayout h = new HelloBorderLayout();
+            h.setVisible(true);
+        });
+    }
+}
